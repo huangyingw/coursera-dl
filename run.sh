aa7216526c2b806297847efb1a98bddb4b868ba8
Ying Huang <huangyingw@gmail.com>
2019-05-28 23:08:19 -0700

    n

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+rm -fr /usr/local/lib/python3.7/site-packages/coursera/
+python3 setup.py build
 python3 setup.py install
+coursera-dl neural-networks-deep-learning
