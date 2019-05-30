02a501c28cc53f274e05c0f6c26969569fc871a4
Ying Huang <huangyingw@gmail.com>
2019-05-29 20:44:50 -0700

    n

--- run.sh
+++ run.sh
@@ -6,4 +6,10 @@ cd "$SCRIPTPATH"
 rm -fr /usr/local/lib/python3.7/site-packages/coursera/
 python3 setup.py build
 python3 setup.py install
-coursera-dl neural-networks-deep-learning
+coursera-dl \
+    -ca 0ZyrSVac6ZlWz5IP4jsS0ElKo0NUjdDTm9v9Arzpg1YNJyIeyRysCxdvbwQ4TfPU051EC7Pu4CDIP0QstvjQQw.0vvoKD4NK_MChItKI4KLkQ.OIEPh1yOMB7iJR1TEl90Dulrn7sEe1qvqXW9BLfMnadD9J_NohPOriMZnnmSlZEHtm00X1BdKG-a1jLwMpblvKqtDmqIHfOljQuhXkjSd6MdSLkja0tHQJ8_74-koNz1vReTwve2u_nRGQYvtBJrXodQ1PNPZWYbjR79RUaX0xO3VeEyFw2KDaUBk5Hk52dD \
+    neural-networks-deep-learning \
+    deep-neural-network \
+    machine-learning-projects \
+    convolutional-neural-networks \
+    nlp-sequence-models
