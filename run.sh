cc99cf30445410c3c37f7fd1323da1c568cd8942
Ying Huang <huangyingw@gmail.com>
2019-11-30 20:37:54 -0800

    n

--- run.sh
+++ run.sh
@@ -17,3 +17,9 @@ coursera-dl \
     machine-learning-projects \
     convolutional-neural-networks \
     nlp-sequence-models
+
+~/loadrc/bashrc/copy.sh download/neural-networks-deep-learning/ deeplearning.ai/1-neural-networks-deep-learning/
+~/loadrc/bashrc/copy.sh download/deep-neural-network/ deeplearning.ai/2-deep-neural-network/
+~/loadrc/bashrc/copy.sh download/machine-learning-projects/ deeplearning.ai/3-machine-learning-projects/
+~/loadrc/bashrc/copy.sh download/convolutional-neural-networks/ deeplearning.ai/4-convolutional-neural-networks/
+~/loadrc/bashrc/copy.sh download/nlp-sequence-models/ deeplearning.ai/5-nlp-sequence-models/
