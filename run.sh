776378842a7e275ae6877a542c66c56def4b9223
Ying Huang <huangyingw@gmail.com>
2019-12-27 11:21:27 -0800

    n

--- run.sh
+++ run.sh
@@ -12,16 +12,18 @@ coursera-dl \
     --download-notebooks \
     --ignore-formats txt \
     --video-resolution 720p \
-    -ca g_k3zN7rt8G58I2VKCHs_0F-hdvMfBO_nXPV-CDVOeWL3F5W2YXu_1JnMzi8IHwRPjvTrGM01tsdg5uE27G3wQ.dBD_0HEqeqmwjEHJzbbleQ.3xNbnP3kPmBQ9_P-rXj-Z5HjRIxUWHTyxRhJSG8ej4kA1ab2KezHot2LvnPmmX6RttDEjHyItTW9S2AyWN_Rdgg3ncuCdKNOYkI917MmRTpddg9CNbDAuQhz9NcpPT0MFWawFaD0HKVL6Wg_WeHI1ShMPQ0lNlw107xktfbsBb2y7Qixy9_yeEKFpO4qS9pD \
+    -ca oBwAh-XKNNIavdrAYbCUxA13mrKB8c0qknI552ZfTCFUMoE6wPkLjo2ZhiyRFGpFnYzH7o8OHKucweW-5eyBIQ.fB1rcq6nX28qHeiXA9xClw.up820keNQgZOFJdGVY-B4FvNkUdYw1DtBK8cuIJMxLSMUYj7b1HAaMAPenwSiIwySnMEXh6aCfo2w1Z5D-AbxFwX9-03rD3M6c7ZlFDgwIJkpnUuMgR3xxwk_jtUbTteyCMOI5j4PvU_ke8PFPmcxc3EQs50TSy-g-BFxX8eE1sUohQk-1WK8usTszfS-lD3 \
     neural-networks-deep-learning \
     deep-neural-network \
     machine-learning-projects \
     convolutional-neural-networks \
-    nlp-sequence-models
+    nlp-sequence-models \
+    machine-learning \
 
 ~/loadrc/bashrc/copy.sh download/neural-networks-deep-learning/ deeplearning.ai/1-neural-networks-deep-learning/
 ~/loadrc/bashrc/copy.sh download/deep-neural-network/ deeplearning.ai/2-deep-neural-network/
 ~/loadrc/bashrc/copy.sh download/machine-learning-projects/ deeplearning.ai/3-machine-learning-projects/
 ~/loadrc/bashrc/copy.sh download/convolutional-neural-networks/ deeplearning.ai/4-convolutional-neural-networks/
 ~/loadrc/bashrc/copy.sh download/nlp-sequence-models/ deeplearning.ai/5-nlp-sequence-models/
+~/loadrc/bashrc/copy.sh download/machine-learning/ deeplearning.ai/6-machine-learning/
 jdupes -1dNr "$TARGET" deeplearning.ai/
