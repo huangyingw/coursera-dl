d706aa618c1026e334ca9483786be948e3eb7009
Ying Huang <huangyingw@gmail.com>
2019-12-28 11:37:25 -0800

    n

--- run.sh
+++ run.sh
@@ -20,10 +20,11 @@ coursera-dl \
     nlp-sequence-models \
     machine-learning \
 
-~/loadrc/bashrc/copy.sh download/neural-networks-deep-learning/ deeplearning.ai/1-neural-networks-deep-learning/
-~/loadrc/bashrc/copy.sh download/deep-neural-network/ deeplearning.ai/2-deep-neural-network/
-~/loadrc/bashrc/copy.sh download/machine-learning-projects/ deeplearning.ai/3-machine-learning-projects/
-~/loadrc/bashrc/copy.sh download/convolutional-neural-networks/ deeplearning.ai/4-convolutional-neural-networks/
-~/loadrc/bashrc/copy.sh download/nlp-sequence-models/ deeplearning.ai/5-nlp-sequence-models/
-~/loadrc/bashrc/copy.sh download/machine-learning/ deeplearning.ai/6-machine-learning/
+~/loadrc/bashrc/copy_ignore_existing.sh download/neural-networks-deep-learning/ deeplearning.ai/1-neural-networks-deep-learning/
+~/loadrc/bashrc/copy_ignore_existing.sh download/deep-neural-network/ deeplearning.ai/2-deep-neural-network/
+~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning-projects/ deeplearning.ai/3-machine-learning-projects/
+~/loadrc/bashrc/copy_ignore_existing.sh download/convolutional-neural-networks/ deeplearning.ai/4-convolutional-neural-networks/
+~/loadrc/bashrc/copy_ignore_existing.sh download/nlp-sequence-models/ deeplearning.ai/5-nlp-sequence-models/
+~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning/ deeplearning.ai/6-machine-learning/
+~/loadrc/htmlrc/convert_all_htmls.sh deeplearning.ai/ 
 jdupes -1dNr "$TARGET" deeplearning.ai/
