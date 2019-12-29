5ffd0eb4610710e013ce6f5bd841d3fafaa9ebfa
Ying Huang <huangyingw@gmail.com>
2019-12-29 14:35:00 -0800

    n

--- post_handle.sh
+++ post_handle.sh
@@ -3,11 +3,12 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-~/loadrc/bashrc/copy_ignore_existing.sh download/neural-networks-deep-learning/ deeplearning.ai/1-neural-networks-deep-learning/
-~/loadrc/bashrc/copy_ignore_existing.sh download/deep-neural-network/ deeplearning.ai/2-deep-neural-network/
-~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning-projects/ deeplearning.ai/3-machine-learning-projects/
-~/loadrc/bashrc/copy_ignore_existing.sh download/convolutional-neural-networks/ deeplearning.ai/4-convolutional-neural-networks/
-~/loadrc/bashrc/copy_ignore_existing.sh download/nlp-sequence-models/ deeplearning.ai/5-nlp-sequence-models/
-~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning/ deeplearning.ai/6-machine-learning/
-~/loadrc/htmlrc/convert_all_htmls.sh ./deeplearning.ai/
-jdupes -1dNr "$TARGET" deeplearning.ai/
+targetFolder="$HOME/Dropbox/myproject/git/AI/coursera/deeplearning.ai/"
+~/loadrc/bashrc/copy_ignore_existing.sh download/neural-networks-deep-learning/ "$targetFolder"1-neural-networks-deep-learning/
+~/loadrc/bashrc/copy_ignore_existing.sh download/deep-neural-network/ "$targetFolder"2-deep-neural-network/
+~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning-projects/ "$targetFolder"3-machine-learning-projects/
+~/loadrc/bashrc/copy_ignore_existing.sh download/convolutional-neural-networks/ "$targetFolder"4-convolutional-neural-networks/
+~/loadrc/bashrc/copy_ignore_existing.sh download/nlp-sequence-models/ "$targetFolder"5-nlp-sequence-models/
+~/loadrc/bashrc/copy_ignore_existing.sh download/machine-learning/ "$targetFolder"6-machine-learning/
+~/loadrc/htmlrc/convert_all_htmls.sh "$targetFolder"
+jdupes -1dNr "$TARGET" "$targetFolder"
