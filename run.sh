359b9892cfd86d67a7977611bcc2419e2beab995
Ying Huang <huangyingw@gmail.com>
2020-06-30 21:01:44 -0700

    n

--- run.sh
+++ run.sh
@@ -12,12 +12,5 @@ coursera-dl \
     --download-notebooks \
     --ignore-formats txt \
     --video-resolution 720p \
-    -ca oBwAh-XKNNIavdrAYbCUxA13mrKB8c0qknI552ZfTCFUMoE6wPkLjo2ZhiyRFGpFnYzH7o8OHKucweW-5eyBIQ.fB1rcq6nX28qHeiXA9xClw.up820keNQgZOFJdGVY-B4FvNkUdYw1DtBK8cuIJMxLSMUYj7b1HAaMAPenwSiIwySnMEXh6aCfo2w1Z5D-AbxFwX9-03rD3M6c7ZlFDgwIJkpnUuMgR3xxwk_jtUbTteyCMOI5j4PvU_ke8PFPmcxc3EQs50TSy-g-BFxX8eE1sUohQk-1WK8usTszfS-lD3 \
-    neural-networks-deep-learning \
-    deep-neural-network \
-    machine-learning-projects \
-    convolutional-neural-networks \
-    nlp-sequence-models \
+    -ca fdsfsd \
     machine-learning
-
-./post_handle.sh
